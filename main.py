868c14b363088d6a18447e81704035f1def166a6
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-11-24 16:18:36 -0500

    Merge conflicts

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-with ""
+FILE_PATH = 'test/test1.txt'
+
+if __name__ == '__main__':
+    for line in open(FILE_PATH):
+        li = line.strip()
+        if not li.startswith("//"):
+            print(line.rstrip())
