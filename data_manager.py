7176653155dccfc0b0860ae84de101d86868c377
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-11-27 15:22:57 -0500

    Parse instructions

--- data_manager.py
+++ data_manager.py
@@ -1,17 +1,17 @@
 class DataManager:
-    def __init__(self, site_idx):
+    def __init__(self, site_id):
         # print("Init Data Manager " + str(site_idx) + "!")
-        self.idx = site_idx
+        self.id = site_id
         self.is_up = True
         self.data = {}
         self.lock_table = {}
         for v_idx in range(1, 21):
             v_name = "x" + str(v_idx)
-            if v_idx % 2 == 0 or v_idx % 10 + 1 == site_idx:
+            if v_idx % 2 == 0 or v_idx % 10 + 1 == site_id:
                 self.data[v_name] = v_idx * 10
 
     def get_instructions(self, instr):
-        print("Data Manager " + str(self.idx) + " received below instruction:")
+        print("Data Manager " + str(self.id) + " received below instruction:")
         print(instr)
 
     def dump(self, idx):
--- transaction_manager.py
+++ transaction_manager.py
@@ -6,14 +6,66 @@ class TransactionManager:
     def __init__(self):
         # print("Init Transaction Manager!")
         self.data_manager_nodes = []
-        for site_idx in range(1, 11):
-            self.data_manager_nodes.append(DataManager(site_idx))
+        for site_id in range(1, 11):
+            self.data_manager_nodes.append(DataManager(site_id))
 
-    def parse_instruction(self, instr):
-        action = re.findall(r"[\w']+", instr)
-        print(action)
-        # self.data_manager_nodes[ran_site].get_instructions(instr)
-        # self.monitor_site_status()
+    def parse_instruction(self, line):
+        instr_list = re.findall(r"[\w']+", line)
+        return {
+            'begin': self.begin,
+            'beginRO': self.beginro,
+            'R': self.read,
+            'W': self.write,
+            'dump': self.dump,
+            'end': self.end,
+            'fail': self.end,
+            'recover': self.end,
+        }.get(instr_list[0], self.invalid_instr)(instr_list)
+
+    # -----------------------------------------------------
+    # -------------- Instruction Executions ---------------
+    # -----------------------------------------------------
+    def begin(self, instr_list):
+        transaction_id = instr_list[1]
+        print(transaction_id + " begins.")
+
+    def beginro(self, instr_list):
+        transaction_id = instr_list[1]
+        print(transaction_id + " begins and is read-only.")
+
+    def read(self, instr_list):
+        transaction_id = instr_list[1]
+        variable = instr_list[2]
+        print(transaction_id + " read " + variable + ".")
+
+    def write(self, instr_list):
+        transaction_id = instr_list[1]
+        variable = instr_list[2]
+        value = instr_list[3]
+        print(transaction_id + " write " + variable +
+              " with value " + value + ".")
+
+    def dump(self, instr_list):
+        print("Dump all data at all sites!")
+
+    def end(self, instr_list):
+        transaction_id = instr_list[1]
+        print(transaction_id + " ends (commits or aborts).")
+
+    def fail(self, instr_list):
+        site_id = instr_list[1]
+        print("Site " + site_id + " fails.")
+
+    def recover(self, instr_list):
+        site_id = instr_list[1]
+        print("Site " + site_id + " recovers.")
+
+    def invalid_instr(self, instr_list):
+        print("Invalid instruction: " + " ".join(instr_list))
+
+    # -----------------------------------------------------
+    # -----------------------------------------------------
+    # -----------------------------------------------------
 
     def monitor_site_status(self):
         for dm in self.data_manager_nodes:
@@ -26,15 +78,9 @@ class TransactionManager:
         print("-------------------- Dump all the output --------------------")
         for dm in self.data_manager_nodes:
             if dm.is_up:
-                print("Site" + str(dm.idx) + " is up")
+                print("Site" + str(dm.id) + " is up")
             else:
-                print("Site" + str(dm.idx) + "'s status: Down")
+                print("Site" + str(dm.id) + "'s status: Down")
 
-            dm_info = dm.dump(dm.idx)
-            print("Site" + str(dm.idx) + "'s data: " + dm_info)
-
-    def fail(self):
-        pass
-
-    def recover(self):
-        pass
+            dm_info = dm.dump(dm.id)
+            print("Site" + str(dm.id) + "'s data: " + dm_info)
