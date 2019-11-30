ec9300bf975c40e549d22c5b9e48ef9f86b3fa68
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-11-30 02:17:04 -0500

    Better variable name

--- transaction_manager.py
+++ transaction_manager.py
@@ -10,7 +10,7 @@ class TransactionManager:
             self.data_manager_nodes.append(DataManager(site_id))
 
     def parse_instruction(self, line):
-        instr_list = re.findall(r"[\w']+", line)
+        instr_line = re.findall(r"[\w']+", line)
         return {
             'begin': self.begin,
             'beginRO': self.beginro,
@@ -20,48 +20,48 @@ class TransactionManager:
             'end': self.end,
             'fail': self.end,
             'recover': self.end,
-        }.get(instr_list[0], self.invalid_instr)(instr_list)
+        }.get(instr_line[0], self.invalid_instr)(instr_line)
 
     # -----------------------------------------------------
     # -------------- Instruction Executions ---------------
     # -----------------------------------------------------
-    def begin(self, instr_list):
-        transaction_id = instr_list[1]
+    def begin(self, instr_line):
+        transaction_id = instr_line[1]
         print(transaction_id + " begins.")
 
-    def beginro(self, instr_list):
-        transaction_id = instr_list[1]
+    def beginro(self, instr_line):
+        transaction_id = instr_line[1]
         print(transaction_id + " begins and is read-only.")
 
-    def read(self, instr_list):
-        transaction_id = instr_list[1]
-        variable = instr_list[2]
+    def read(self, instr_line):
+        transaction_id = instr_line[1]
+        variable = instr_line[2]
         print(transaction_id + " read " + variable + ".")
 
-    def write(self, instr_list):
-        transaction_id = instr_list[1]
-        variable = instr_list[2]
-        value = instr_list[3]
+    def write(self, instr_line):
+        transaction_id = instr_line[1]
+        variable = instr_line[2]
+        value = instr_line[3]
         print(transaction_id + " write " + variable +
               " with value " + value + ".")
 
-    def dump(self, instr_list):
+    def dump(self, instr_line):
         print("Dump all data at all sites!")
 
-    def end(self, instr_list):
-        transaction_id = instr_list[1]
+    def end(self, instr_line):
+        transaction_id = instr_line[1]
         print(transaction_id + " ends (commits or aborts).")
 
-    def fail(self, instr_list):
-        site_id = instr_list[1]
+    def fail(self, instr_line):
+        site_id = instr_line[1]
         print("Site " + site_id + " fails.")
 
-    def recover(self, instr_list):
-        site_id = instr_list[1]
+    def recover(self, instr_line):
+        site_id = instr_line[1]
         print("Site " + site_id + " recovers.")
 
-    def invalid_instr(self, instr_list):
-        print("Invalid instruction: " + " ".join(instr_list))
+    def invalid_instr(self, instr_line):
+        print("Invalid instruction: " + " ".join(instr_line))
 
     # -----------------------------------------------------
     # -----------------------------------------------------
