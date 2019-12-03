47e4056845b80d9f3cca645689a486821cb8bffa
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-12-03 11:35:26 -0500

    Minor

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         # print("---------------- Begin to process instructions ----------------")
         for line in fh:
             if not tm.process_line(line):
-                print("Abort!")
+                print("[Abort!]")
                 break
