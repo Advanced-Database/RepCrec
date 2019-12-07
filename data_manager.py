832ace3e3acf25c4a9bf207568950651b5b4fbec
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-12-07 18:58:50 -0500

    Rewrite locks and fix dm.read

--- data_manager.py
+++ data_manager.py
@@ -13,6 +13,7 @@ class Variable:
         self.committed_value_list = [init_value]  # latest commit at front
         self.is_replicated = is_replicated
         self.temp_value = None
+        self.is_readable = True
 
     def get_last_committed_value(self):
         return self.committed_value_list[0].value
@@ -34,28 +35,51 @@ class LockType(Enum):
     W = 2
 
 
+class ReadLock:
+    def __init__(self, variable_id, transaction_id):
+        self._variable_id = variable_id
+        self.transaction_id_set = {transaction_id}
+        self.lock_type = LockType.R
+
+
+class WriteLock:
+    def __init__(self, variable_id, transaction_id):
+        self._variable_id = variable_id
+        self.transaction_id = transaction_id
+        self.lock_type = LockType.W
+
+
 class QueuedLock:
     def __init__(self, variable_id, transaction_id, lock_type: LockType):
         self._variable_id = variable_id
         self.transaction_id = transaction_id
         self.lock_type = lock_type
 
-# todo: rewrite locks
-class Lock:
-    def __init__(self, variable_id, transaction_id, lock_type: LockType):
+
+class LockManager:
+    def __init__(self, variable_id):
         self._variable_id = variable_id
-        self.transaction_list = {transaction_id}
-        self.lock_type = lock_type
-        self.queue = []
+        self.current_lock = None
+        self.queue = []  # list of QueuedLock
 
-    def share_read(self, transaction_id):
-        if not self.lock_type == LockType.R:
+    def set_read_lock(self, read_lock):
+        if self.queue:
+            raise RuntimeError(
+                "Unresolved queued locks when current lock is None!")
+        self.current_lock = read_lock
+
+    def share_read_lock(self, transaction_id):
+        if not self.current_lock.lock_type == LockType.R:
             raise RuntimeError("Attempt to share W-lock!")
-        self.transaction_list.add(transaction_id)
+        self.current_lock.transaction_id_set.add(transaction_id)
 
-    def add_to_queue(self, queued_lock):
-        # todo: avoid duplicates
-        self.queue.append(queued_lock)
+    def add_to_queue(self, new_lock: QueuedLock):
+        for queued_lock in self.queue:
+            if queued_lock.transaction_id == new_lock.transaction_id:
+                if queued_lock.lock_type == new_lock.lock_type or \
+                        new_lock.lock_type == LockType.R:
+                    return
+        self.queue.append(new_lock)
 
     def has_queued_write_lock(self):
         for queued_lock in self.queue:
@@ -75,11 +99,11 @@ class DataManager:
             if v_idx % 2 == 0:
                 self.data[variable_id] = Variable(
                     variable_id, CommitValue(v_idx * 10, 0), True)
-                # self.lock_table[variable_id] = None
+                self.lock_table[variable_id] = LockManager(variable_id)
             elif v_idx % 10 + 1 == self.site_id:
                 self.data[variable_id] = Variable(
                     variable_id, CommitValue(v_idx * 10, 0), False)
-                # self.lock_table[variable_id] = None
+                self.lock_table[variable_id] = LockManager(variable_id)
 
         self.fail_ts_list = []  # latest fail at end
         self.recover_ts_list = []  # latest recover at end
@@ -102,35 +126,33 @@ class DataManager:
 
     def read(self, transaction_id, variable_id):
         v = self.data.get(variable_id)
-        if v:
-            lock = self.lock_table.get(variable_id)
-            if lock:
-                if lock.lock_type == LockType.R:
-                    if transaction_id in lock.transaction_list:
+        # todo: site just recovered, variable not readable
+        if v and v.is_readable:
+            lock_manager: LockManager = self.lock_table[variable_id]
+            current_lock = lock_manager.current_lock
+            if current_lock:
+                if current_lock.lock_type == LockType.R:
+                    if transaction_id in current_lock.transaction_id_set:
                         return Result(True, v.get_last_committed_value())
-                    if not lock.has_queued_write_lock():
-                        lock.share_read(transaction_id)
+                    if not lock_manager.has_queued_write_lock():
+                        lock_manager.share_read_lock(transaction_id)
                         return Result(True, v.get_last_committed_value())
                     # There is a queued W-lock
-                    lock.add_to_queue(
+                    lock_manager.add_to_queue(
                         QueuedLock(variable_id, transaction_id, LockType.R))
                     return Result(False)
-                # lock.lock_type == LockType.W
-                if len(lock.transaction_list) != 1:
-                    raise RuntimeError("Multiple transaction holding W-lock"
-                                       " on the same variable!")
-                if transaction_id in lock.transaction_list:
+                # current_lock.lock_type == LockType.W
+                if transaction_id == current_lock.transaction_id:
                     # This transaction holds a W-lock
                     # It has written to the variable
                     # but the new value is not committed yet
                     return Result(True, v.get_temp_value())
                 # Another transaction is holding a W-lock
-                lock.add_to_queue(
-                    QueuedLock(variable_id, transaction_id, LockType.W))
+                lock_manager.add_to_queue(
+                    QueuedLock(variable_id, transaction_id, LockType.R))
                 return Result(False)
             # No existing lock on the variable, create one
-            self.lock_table[variable_id] = Lock(
-                variable_id, transaction_id, LockType.R)
+            lock_manager.set_read_lock(ReadLock(variable_id, transaction_id))
             return Result(True, v.get_last_committed_value())
         return Result(False)
 
