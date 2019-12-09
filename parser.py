263f8cdd16c4a265f70e68cf8ddab32ae2c75288
Zheng Jiang <zhengjiangdqr@gmail.com>
2019-12-09 15:02:23 -0500

    Fix parser docstring

--- parser.py
+++ parser.py
@@ -5,11 +5,11 @@ class Parser:
     debug_info_below = False
 
     def parse_line(self, line):
-        '''
-        parse and split each line into multiple sub-strings as the command and arguments
-        :param line: each line of the input file
-        :return: a list of strings of the command and arguments
-        '''
+        """
+        Parse one line of input into command and arguments.
+        :param line: one line of input
+        :return: a list of strings consisting of command and arguments
+        """
         if self.debug_info_below:
             return None
         li = line.split('//')[0].strip()
