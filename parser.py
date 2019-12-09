d095772132796a9c60c5cf75732d947359a77cd1
Ian Chou <ianchou19@gmail.com>
2019-12-09 13:31:21 -0500

    Add comment for parser module

--- parser.py
+++ parser.py
@@ -5,6 +5,11 @@ class Parser:
     debug_info_below = False
 
     def parse_line(self, line):
+        '''
+        parse and split each line into multiple sub-strings as the command and arguments
+        :param line: each line of the input file
+        :return: a list of strings of the command and arguments
+        '''
         if self.debug_info_below:
             return None
         li = line.split('//')[0].strip()
